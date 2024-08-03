28abea7bd90aaf8de74d0c3699d0be5d44d2e677
Pham Duc Minh <95753855+Deemen17@users.noreply.github.com>
2024-08-03 13:02:07 +0000

    Update Talice

--- keyboards/talice/old_files/config copy.h
+++ keyboards/talice/old_files/config copy.h
@@ -42,6 +42,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define MATRIX_ROW_PINS { B4, B5, B6, C6, C7 }
 #define MATRIX_COL_PINS { E2, B1, B3, F0, F1, F4, F5, F6, B7, D5, D7, D6, D3, D2, D4 }
+
+        // "cols": ["E2", "B1", "B3", "F0", "F1", "F4", "F5", "F6", "B7", "D5", "D7", "D6", "D3", "D2", "D4"],
+        // "rows": ["B4", "B5", "B6", "C6", "C7"]
+
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
