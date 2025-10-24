066c023f8aafe1cea65688d040b5fc96598ea94b
Deemen17 <deemen17@gmail.com>
2025-10-24 17:19:41 +0700

    Add underglow for Neo65 Tri Mode

---
+++ keyboards/neo/neo65_trimode/add_underglow/add_underglow.c
@@ -0,0 +1,8 @@
+// #include "keyboards/neo/neo65_trimode/neo65_trimode.c"
+
+// // Temporary work around for WS2812 pin init
+// void board_init(void) {
+//     gpio_set_pin_output(WS2812_DI_PIN);
+//     gpio_write_pin_low(WS2812_DI_PIN);
+// }
+
